ad52329811fd1fc6792021f72e93b5f9ebcd033d
Theivndu <23perert@elmbrookstudents.org>
2020-11-07 17:16:43 -0600

    Random Test Change

--- src/main/java/com/swervedrivespecialties/exampleswerve/Main.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/Main.java
@@ -4,7 +4,10 @@ package com.swervedrivespecialties.exampleswerve;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Main {
-    public static void main(String... args) {
+    public static void main(String... args) 
+    {
         RobotBase.startRobot(Robot::new);
+        //hi
+        System.out.print("Hi");
     }
 }
